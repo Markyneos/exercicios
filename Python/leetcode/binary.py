ad74e4a27197c74d26b3d58ff5278a856406b3ca
Markyneos <mafaustine18@gmail.com>
2024-10-18 16:20:30 -0300

    Consegui realizar o exercício e fazer o código horroroso ficar melhor e funcionar

--- Python/leetcode/binary.py
+++ Python/leetcode/binary.py
@@ -1,24 +1,17 @@
 class Solution:
   def searchInsert(self, nums: list[int], target: int) -> int:
-    start = 0
-    end = len(nums) - 1
-    mid = end // 2
-    while True:
-      if target == nums[mid]:
+    start, end = 0, len(nums) - 1
+    while start <= end:
+      mid = (start + end) // 2
+      if nums[mid] < target:
+        start = mid + 1
+      elif nums[mid] > target:
+        end = mid - 1
+      else:
         return mid
-      elif target > nums[mid]:
-        start = mid
-        mid = start + end // 2
-      elif target < nums[mid]:
-        end = mid
-        mid = start + end // 2
-      if target > nums[mid] and target < nums[mid + 1]:
-        return mid + 1
-      if target < nums[mid] and target > nums[mid - 1]:
-        return mid - 1
-
+    return start
 
 nums = [1, 3, 5, 6]
-target = 2
+target = 7
 objeto = Solution()
 print(objeto.searchInsert(nums, target))
