6781fdaab31222179f01a7f74317abb929023322
Markyneos <mafaustine18@gmail.com>
2025-08-13 02:31:51 -0300

    Fiz um novo leet

---
+++ Python/leetcode/pascalsTriangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        resultado = []
+
+        for i in range(numRows):
+            temp = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    temp.append(1)
+                elif j == 1 or j == i - 1:
+                    temp.append(i)
+                else:
+                    temp.append(resultado[i - 1][j] + resultado[i - 1][j - 1])
+            resultado.append(temp)
+        return resultado
